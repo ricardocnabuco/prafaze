7162b0539630465967d6d80d9ff556f460b15909
Ricardo de Carvalho Nabuco <ricardocnabuco@gmail.com>
2023-09-28 14:16:58 -0300

    NOVAS ALTERACOES

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,70 +1,67 @@
-import Head from 'next/head';
-import styles from '../styles/home.module.css';
+
+import Head from 'next/head'
+import styles from '@/styles/Home.module.css'
+import Image from 'next/image'
+
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-        <div className={styles.container2}>
-          PraFazê!<img src="/clipboard-text.png" alt="tarefa">
-        </div>
-      
-        <div className={styles.container}>
-           <div className={styles.tela-principal}>
-  
-           <form action="" method="">
-             <input className={styles.button}  name="incluir" type="submit" value=" Incluir " />
-              <div className={styles.input}><input name="tarefa" type="text" placeholder="Digite nova tarefa :)" /></div>
-           </form>
 
-           <div className={styles.traco}></div>
+  <div className={styles.container}>
+    <section className={styles.titulo}>
+        <span>PraFazê</span>
+         <img className={styles.imgPraFaze}></img>
      
-      <div>
-        <input type="checkbox" id="tarefa1" name="tarefa1" />
+
+
+    </section>
+    <div className={styles.telaPrincipal}>
+      <form action="" method="">
+        <input className={styles.button}  name="incluir" type="submit" value=" Incluir " />
+        <div className={styles.input}><input name="tarefa" type="text" placeholder="Digite uma nova tarefa :)" />
+        </div>
+      </form>
+      <div className={styles.traco}></div>
+      <section className={styles.box}>
+        <input type="checkbox" id="tarefa1" name="tarefa1" />&nbsp;&nbsp;
         <label for="tarefa1">Sou uma tarefa :)</label>
-      </div>
+      </section>
       <div>
-        <input type="checkbox" id="tarefa2" name="tarefa2" />
+        <input type="checkbox" id="tarefa2" name="tarefa2" />&nbsp;&nbsp;
         <label for="tarefa2">Sou uma tarefa :)</label>
       </div>
       <div>
-        <input type="checkbox" id="tarefa3" name="tarefa3" />
+        <input type="checkbox" id="tarefa3" name="tarefa3" />&nbsp;&nbsp;
         <label for="tarefa3">Sou uma tarefa :)</label>
       </div>
       <div>
-        <input type="checkbox" id="tarefa4" name="tarefa4" />
-        <label for="tarefa4">Sou uma tarefa :)</label>
-      </div>
-      <div>
-        <input type="checkbox" id="tarefa5" name="tarefa5" />
+        <input type="checkbox" id="tarefa5" name="tarefa5" />&nbsp;&nbsp;
         <label for="tarefa5">Sou uma tarefa :)</label>
       </div>
       <div>
-        <input type="checkbox" id="tarefa6" name="tarefa6" checked />
+        <input type="checkbox" id="tarefa6" name="tarefa6" checked />&nbsp;&nbsp;
         <label for="tarefa6">Sou uma tarefa :)</label>
-      </div>
-      <div>
-        <input type="checkbox" id="tarefa7" name="tarefa7" />
-        <label for="tarefa7">Sou uma tarefa :)</label>
-      </div>
-      <div>
-        <input type="checkbox" id="tarefa8" name="tarefa8" />
-        <label for="tarefa8">Sou uma tarefa :)</label>
-      </div>
-      <div>
-        <input type="checkbox" id="tarefa9" name="tarefa9" />
-        <label for="tarefa9">Sou uma tarefa :)</label>
       </div>
       <div className={styles.traco}></div> 
 
-      <div className={styles.footer}>
-        <p>5 itens restantes&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <b>Tudo</b>&nbsp;&nbsp;&nbsp;&nbsp;Ativas&nbsp;&nbsp;&nbsp;&nbsp;
-          Completas&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Limpar completas</p>
-      </div>
-      
+      <section className={styles.button2}>
+          <span>4 itens restantes</span>
+      </section>  
+      <section className={styles.button2}>
+          <span>Tudo</span>
+      </section>  
+      <section className={styles.button2}>
+          <span>Ativas</span>
+      </section> 
+      <section className={styles.button2}>
+          <span>Completas</span>
+      </section> 
+      <section className={styles.button2}>
+          <span>Limpar Completas</span>
+      </section> 
+
+    </div>
   </div>
-</div>
-   
-   </main>
-  );
+
+  )
 }
